4d4d91c82ebcac9f8d014ede6f0b0d2a8e7bddd2
antonioditcharo <antonio.ditcharo@gmail.com>
2024-12-22 10:51:11 -0800

    Added My Little Plugin, changed rootProject.name, changed runelite-plugin.properties values.

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/my_little_plugin/src/test/java/com/example/ExamplePluginTest.java
@@ -0,0 +1,14 @@
+package net.runelite.client.plugins.my_little_plugin.src.test.java.com.example;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+import net.runelite.client.plugins.my_little_plugin.ExamplePlugin;
+
+public class ExamplePluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		RuneLite.main(args);
+	}
+}
